2482c7fab241233e93c1484487090c529fbc5278
Nikola Surjanovic <16364942+nikola-sur@users.noreply.github.com>
2021-12-16 14:36:22 -0800

    Add RcppArmadillo to project

--- src/predict_CLDA_cpp.cpp
+++ src/predict_CLDA_cpp.cpp
@@ -1,20 +1,34 @@
+#include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppArmadillo)]]
 #include <Rcpp.h>
 using namespace Rcpp;
 
 // [[Rcpp::export]]
 NumericVector predict_CLDA_cpp(List mod, NumericMatrix x) {
   // Collect inputs ---
+  int n = x.nrow();
+  int p = x.ncol();
   NumericVector beta = mod["beta"];
-  NumericMatrix Sigma_w = mod["Sigma_w"];
   NumericVector xbar_0 = mod["xbar_0"];
   NumericVector xbar_1 = mod["xbar_1"];
-  int n = x.nrow();
-  
+  NumericMatrix Sigma_w = mod["Sigma_w"];
+  NumericMatrix x_old = Rcpp::clone(x);
+  // arma::mat x = x_old;
   
   // Make predictions
   NumericVector preds(n);
+  NumericVector obs(p);
+  NumericVector diffs_0(p);
+  NumericVector diffs_1(p);
   
   for (int i = 0; i < n; i++) {
+    for (int j = 0; j < p; j++) { // Retrieve the i-th row
+      obs[j] = x(i, j);
+    }
+    
+    diffs_0 = obs - xbar_0;
+    diffs_1 = obs - xbar_1;
+    
     preds[i] = 0.0;
   }
   
