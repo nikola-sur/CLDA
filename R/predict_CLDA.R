f8785b538aa05637e13f1f63a6e4faa050398e79
Nikola Surjanovic <16364942+nikola-sur@users.noreply.github.com>
2021-12-16 20:57:50 -0800

    Make QDA a bit faster

--- R/predict_CLDA.R
+++ R/predict_CLDA.R
@@ -19,27 +19,35 @@ predict.CLDA <- function(mod, x) {
   # Make predictions ---
   preds <- numeric(n_test)
   
+  diffs_0_mat <- sweep(x = x, MARGIN = 2, STATS=mod$xbar_0, FUN = "-")
+  diffs_1_mat <- sweep(x = x, MARGIN = 2, STATS=mod$xbar_1, FUN = "-")
+  
+  if (mod$params$linear) { # LDA
+    if (mod$params$type == "projected") {
+      inv_val <- mod$inv_val
+    } else {
+      inv_val <- 1.0 / (t(mod$beta) %*% mod$Sigma_w %*% mod$beta)
+    }
+  } else { # QDA
+    Sigma_w_diffs_0 <- solve(mod$Sigma_w_0, t(diffs_0_mat))
+    Sigma_w_diffs_1 <- solve(mod$Sigma_w_1, t(diffs_1_mat))
+  }
+  
+  
   for (i in 1:n_test) { # Will be slow in R, but RcppArmadillo had some compilation issues...
     if (mod$params$type %in% c("full", "compressed", "projected", "subsampled")) {
-      obs <- x[i, ]
-      diffs_0 <- obs - mod$xbar_0
-      diffs_1 <- obs - mod$xbar_1
+      diffs_0 <- diffs_0_mat[i, ]
+      diffs_1 <- diffs_1_mat[i, ]
       
       if (mod$params$linear) { # LDA
         beta_diffs_0 <- t(mod$beta) %*% diffs_0
         beta_diffs_1 <- t(mod$beta) %*% diffs_1
-        if (mod$params$type == "projected") {
-          inv_val <- mod$inv_val
-        } else {
-          inv_val <- 1.0 / (t(mod$beta) %*% mod$Sigma_w %*% mod$beta)
-        }
-        
         val0 <- beta_diffs_0 * inv_val * beta_diffs_0 - 2*log(mod$n_0/mod$n)
         val1 <- beta_diffs_1 * inv_val * beta_diffs_1 - 2*log(mod$n_1/mod$n)
       } else { # QDA
-        val0 <- t(diffs_0) %*% solve(mod$Sigma_w_0, diffs_0) + 
+        val0 <- t(diffs_0) %*% Sigma_w_diffs_0[, i] + 
           determinant(mod$Sigma_w_0, logarithm=TRUE)$modulus[1] - 2 * log(mod$pi_0)
-        val1 <- t(diffs_1) %*% solve(mod$Sigma_w_1, diffs_1) + 
+        val1 <- t(diffs_1) %*% Sigma_w_diffs_1[, i] + 
           determinant(mod$Sigma_w_1, logarithm=TRUE)$modulus[1] - 2 * log(mod$pi_1)
       }
     }
