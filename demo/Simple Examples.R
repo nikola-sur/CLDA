53581bc3d4225f44acde2bc7638b956048a33e41
Nikola Surjanovic <16364942+nikola-sur@users.noreply.github.com>
2021-12-17 14:40:29 -0800

    Add subsampled QDA

--- demo/Simple Examples.R
+++ demo/Simple Examples.R
@@ -23,6 +23,7 @@ mod_L4 <- CLDA::CLDA(x = x, y = y, linear = TRUE, type = "subsampled", m = floor
 
 mod_Q1 <- CLDA::CLDA(x = x, y = y, linear = FALSE, type = "full", gamma = 1e-4)
 mod_Q2 <- CLDA::CLDA(x = x, y = y, linear = FALSE, type = "compressed", m = floor(nrow(x)/10), s = 0.01, gamma = 1e-4)
+mod_Q3 <- CLDA::CLDA(x = x, y = y, linear = FALSE, type = "subsampled", m = floor(nrow(x)/10), gamma = 1e-4)
 
 
 # Make predictions
@@ -34,3 +35,4 @@ preds_L4 <- predict.CLDA(mod = mod_L4, x = x_test)
 
 preds_Q1 <- predict.CLDA(mod = mod_Q1, x = x_test)
 preds_Q2 <- predict.CLDA(mod = mod_Q2, x = x_test)
+preds_Q3 <- predict.CLDA(mod = mod_Q3, x = x_test)
