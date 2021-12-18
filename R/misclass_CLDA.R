9b7c8b185c60b25a721ad2ef73ece85946ab7ae8
Nikola Surjanovic <16364942+nikola-sur@users.noreply.github.com>
2021-12-17 16:41:05 -0800

    Create misclass_CLDA.R

---
+++ R/misclass_CLDA.R
@@ -0,0 +1,14 @@
+#' Compute misclassification error rates
+#'
+#' @param preds Vector of predictions
+#' @param y Vector of true values
+#'
+#' @return
+#' @export
+misclass.CLDA <- function(preds, y) {
+  stopifnot(sum(preds %in% c(0, 1)) == length(preds))
+  stopifnot(sum(y %in% c(0, 1)) == length(y))
+  stopifnot(length(preds) == length(y))
+  
+  return(mean(preds == y))
+}
