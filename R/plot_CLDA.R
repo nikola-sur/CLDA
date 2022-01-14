660e83dc53be3a7c19482bf5da8e8df04f6dd869
Nikola Surjanovic <16364942+nikola-sur@users.noreply.github.com>
2022-01-14 14:56:19 -0800

    Update plots

--- R/plot_CLDA.R
+++ R/plot_CLDA.R
@@ -8,26 +8,30 @@
 plot.CLDA <- function(data, light_theme = TRUE) {
   # Transform data for plotting ---
   data$method <- factor(data$method, levels = c("L_compressed", "L_projected", "L_FRF", "L_subsampled", "L_full",
-                                                "Q_compressed", "Q_subsampled", "Q_full"))
+                                                "Q_compressed", "Q_subsampled", "Q_full"),
+                        labels = c("Compressed LDA", "Projected LDA", "Fast Random Fisher", "Sub-sampled LDA", "Full LDA",
+                                   "Compressed QDA", "Subs-sampled QDA", "Full QDA"))
   m_vec <- unique(data$m)[order(unique(data$m))]
   data$m <- match(data$m, m_vec)
   
   
   # LDA misclassification error rates ---
-  p1 <- ggplot2::ggplot(subset(data, method %in% c("L_compressed", "L_projected", "L_FRF", "L_subsampled")), 
+  full_LDA_error <- mean(data$error[data$method == "Full LDA"])
+  p1 <- ggplot2::ggplot(subset(data, method %in% c("Compressed LDA", "Projected LDA", "Fast Random Fisher", "Sub-sampled LDA")), 
                         ggplot2::aes(x = m, y = error, col = method, group = interaction(method, m))) +
     ggplot2::geom_boxplot() +
     ggplot2::xlab("Reduced Sample Amount m") + 
     ggplot2::ylab("Misclassification Error Rates") +
     ggplot2::scale_x_continuous(breaks = unique(data$m), minor_breaks = NULL,
                                 labels = m_vec) +
-    ggplot2::labs(color = "Method")
+    ggplot2::labs(color = "Method") +
+    ggplot2::geom_hline(yintercept = full_LDA_error, linetype = "dashed", size = 1.0)
   if (light_theme) p1 <- p1 + ggplot2::theme_light()
   plot(p1)
   
   
   # LDA execution times ---
-  p2 <- ggplot2::ggplot(subset(data, method %in% c("L_compressed", "L_full")), 
+  p2 <- ggplot2::ggplot(subset(data, method %in% c("Compressed LDA", "Full LDA")), 
                         ggplot2::aes(x = m, y = exec_time, col = method, group = interaction(method, m))) +
     ggplot2::geom_boxplot() +
     ggplot2::xlab("Reduced Sample Amount m") + 
@@ -40,7 +44,7 @@ plot.CLDA <- function(data, light_theme = TRUE) {
   
   
   # QDA misclassification error rates ---
-  p3 <- ggplot2::ggplot(subset(data, method %in% c("Q_compressed", "Q_subsampled")), 
+  p3 <- ggplot2::ggplot(subset(data, method %in% c("Compressed QDA", "Sub-sampled QDA")), 
                         ggplot2::aes(x = m, y = error, col = method, group = interaction(method, m))) +
     ggplot2::geom_boxplot() +
     ggplot2::xlab("Reduced Sample Amount m") + 
